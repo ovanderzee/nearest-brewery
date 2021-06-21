cf0534f69c15bc9d8bed84d3c914353bbf54ef5b
Onno van der Zee <hastalavista@dds.nl>
2021-06-21 15:39:18 +0200

    vue add typescript (v3/typescript-support)
    ? Use class-style component syntax? No
    ? Use Babel alongside TypeScript (required for modern mode, auto-detected polyfi
    lls, transpiling JSX)? Yes
    ? Convert all .js files to .ts? Yes
    ? Allow .js files to be compiled? Yes
    ? Skip type checking of all declaration files (recommended for apps)? No

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,27 @@
 <template>
-  <BubbleBox />
-  <main>
-    <h1>NeighBeerGood</h1>
-    <SearchBrewery />
-  </main>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
-<script>
-import BubbleBox from "./components/BubbleBox.vue";
-import SearchBrewery from "./components/SearchBrewery.vue";
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
+export default defineComponent({
   name: "App",
   components: {
-    BubbleBox,
-    SearchBrewery,
+    HelloWorld,
   },
-};
+});
 </script>
 
 <style>
-:root {
-  --whitish-beer: hsl(43, 100%, 90%);
-  --opaque-strong-beer: hsl(39, 76%, 62%, 0.67);
-  --darkish-beer: hsl(25, 25%, 44%);
-}
-
-body {
-  margin: 0;
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 1.333em;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  background: linear-gradient(
-    to right,
-    hsl(25, 32%, 56%) 0%,
-    hsl(40, 55%, 62%) 9%,
-    hsl(43, 100%, 75%) 27%,
-    hsl(42, 100%, 68%) 45%,
-    hsl(39, 76%, 62%) 83%,
-    hsl(38, 58%, 57%) 91%,
-    hsl(19, 35%, 63%) 100%
-  );
-  overflow: hidden;
-}
-main {
-  padding: 0 4em;
-  overflow: auto;
-  height: 100vh;
-}
-h1 {
-  margin: 0;
-  padding: 2em 0;
-  font-size: 2em;
   text-align: center;
-}
-@media only screen and (orientation: landscape) {
-  h1 {
-    text-align: left;
-  }
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
